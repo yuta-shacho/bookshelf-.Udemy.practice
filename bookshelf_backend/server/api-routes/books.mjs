e3b80517bb0146cca6a7da18bfb74c7d088477a0
yuta3268-shacho <yuta-soccer-3268@docomo.ne.jp>
2024-08-20 05:09:58 +0900

    リクエストのバリデーションとリファクタリング

--- bookshelf_backend/server/api-routes/books.mjs
+++ bookshelf_backend/server/api-routes/books.mjs
@@ -1,65 +1,40 @@
 import express from "express";
-import { body, validationResult } from "express-validator";
-import Book from "../models/book.mjs";
+import { body } from "express-validator";
+
+import {
+  getAllBooks,
+  getBookById,
+  deleteBook,
+  registBook,
+  updateBook,
+} from "../contorollers/books.mjs";
 
 const router = express.Router();
 
 // /api/books
-router.get("/", async (req, res) => {
-  const books = await Book.find().sort({ updatedAt: -1 });
-  res.json(books);
-});
+router.get("/", getAllBooks);
 
-router.get("/:id", async (req, res) => {
-  const _id = req.params.id;
-  const books = await Book.find({ _id: _id });
-  res.json(books);
-});
+router.get("/:id", getBookById);
 
-router.delete("/:id", async (req, res) => {
-  const _id = req.params.id;
-  await Book.deleteOne({ _id: _id });
-  res.json({ msg: "Delete succeeded." });
-});
+router.delete("/:id", deleteBook);
 
 router.post(
   "/",
-  body("title").notEmpty().withMessage("エラーメッセージ"),
-  async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      const errs = errors.array();
-      return res.status(400).json(errs);
-    }
-    const body = req.body;
-    const book = new Book(req.body);
-    const newBook = await book.save();
-    res.json(newBook);
-  }
+  body("title").notEmpty(),
+  body("description").notEmpty(),
+  body("comment").notEmpty(),
+  body("rating").notEmpty().isInt({ min: 1, max: 5 }),
+  registBook
 );
 
-router.patch("/:id", async (req, res) => {
-  const { title, description, comment, rating } = req.body;
-  const _id = req.params.id;
-  const book = await Book.findById(_id);
-  if (title !== undefined) {
-    book.title = title;
-  }
-
-  if (description !== undefined) {
-    book.description = description;
-  }
-
-  if (comment !== undefined) {
-    book.comment = comment;
-  }
-
-  if (rating !== undefined) {
-    book.rating = rating;
-  } //送らせてきたのがかけてても値を更新できるようにする処理
-  await book.save();
-  res.json(book);
-});
+//validator.js
+router.patch(
+  "/:id",
+  body("title").optional().notEmpty(),
+  body("description").optional().notEmpty(),
+  body("comment").optional().notEmpty(),
+  body("rating").optional().notEmpty().isInt({ min: 1, max: 5 }),
+  updateBook
+);
 
 export default router;
