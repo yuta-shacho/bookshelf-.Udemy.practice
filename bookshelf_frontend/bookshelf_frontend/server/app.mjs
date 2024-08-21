c8bbd00b60c38b81bbc24e5e86e3ac49551250d1
yuta3268-shacho <yuta-soccer-3268@docomo.ne.jp>
2024-08-21 16:54:48 +0900

    変更と更新とcorsの処理

---
+++ bookshelf_frontend/bookshelf_frontend/server/app.mjs
@@ -0,0 +1,37 @@
+import express from "express";
+import apiRoutes from "./api-routes/index.mjs";
+import env from "dotenv";
+import "./helpers/db.mjs";
+env.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+//json受け取り
+app.use(express.json());
+
+import cors from "cors";
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+//API
+app.use("/api", apiRoutes);
+
+app.use(function (req, res) {
+  res.status(404).json({ msg: "Page Not Found" });
+});
+
+//エラーハンドラー
+app.use(function (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(500).json({ msg: "不正なエラーが見つかりました。" });
+});
+
+app.listen(port, function () {
+  console.log(`Sever Start: http://localhost:${port}`);
+});
